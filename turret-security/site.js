00153d52762f173e2bb0a14130fb812e0f08e37d
Will Boyd <will@codersblock.com>
2016-04-12 21:08:16 -0400

    Audio events, better kill detection

--- turret-security/site.js
+++ turret-security/site.js
@@ -2,6 +2,12 @@ var video = $('video')[0];
 var $motionBox = $('.motion-box');
 var $turret = $('img');
 
+var scale = 10;	// capture resolution over motion resolution
+var isTargetLost = true;
+var isKilled = false;
+
+var lostTimeout;
+
 function initSuccess() {
 	DiffCamEngine.start();
 }
@@ -10,13 +16,21 @@ function initError() {
 	alert('Something went wrong.');
 }
 
+function startComplete() {
+	play('activated');
+}
+
 function capture(payload) {
+	if (isKilled) {
+		return;
+	}
+
 	var box = payload.motionBox;
 	if (box) {
-		var left = box.x.min;
-		var top = box.y.min;
-		var width = box.x.max - box.x.min;
-		var height = box.y.max - box.y.min;
+		var left = box.x.min * scale + 1;
+		var top = box.y.min * scale + 1;
+		var width = (box.x.max - box.x.min) * scale;
+		var height = (box.y.max - box.y.min) * scale;
 
 		$motionBox.css({
 			display: 'block',
@@ -26,27 +40,45 @@ function capture(payload) {
 			height: height
 		});
 
-		// the extra width/height checks weed out full frame motion
-		// caused by a webcam's initial lighting adjustment
-		if (left < 140 && top < 80 && width < 639 && height < 479) {
-			killTurret();
+		if (isTargetLost) {
+			play('i-see-you');
+			isTargetLost = false;
+		} else {
+			play('fire');
 		}
+		clearTimeout(lostTimeout);
+		lostTimeout = setTimeout(declareLost, 2000);
+	}
+
+	if (payload.checkMotionPixel(0, 0)) {
+		killTurret();
 	}
 }
 
+function play(audioId) {
+	$('#audio-' + audioId)[0].play();
+	console.log(audioId);
+}
+
+function declareLost() {
+	play('target-lost');
+	isTargetLost = true;
+}
+
 function killTurret() {
-	$turret.addClass('kill');
-	$('#audio-ow')[0].play();
+	isKilled = true;
+	clearTimeout(lostTimeout);
+	$motionBox.hide();
+	$turret.addClass('killed');
+	play('ow');
 }
 
 DiffCamEngine.init({
 	video: video,
-	diffWidth: 640,
-	diffHeight: 480,
-	pixelDiffThreshold: 50,
-	scoreThreshold: 500,
 	includeMotionBox: true,
+	includeMotionPixels: true,
 	initSuccessCallback: initSuccess,
 	initErrorCallback: initError,
+	startCompleteCallback: startComplete,
 	captureCallback: capture
 });
